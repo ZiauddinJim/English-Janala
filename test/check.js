e333a9cb3d9d61e17cce88c13f248d8db867f222
SheikhMdZiauddin <ziauddinjim@outlook.com>
2025-09-06 18:05:55 +0600

    same topic update

--- test/check.js
+++
@@ -1,6 +0,0 @@
-function pronounceWord(word) {
-  console.log(word);
-  const utterance = new SpeechSynthesisUtterance(word);
-  utterance.lang = "en-EN"; // Japanese
-  window.speechSynthesis.speak(utterance);
-}
